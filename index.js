0262cfb93d3dc72ce0ceed078ed4eeeaf74da300
Peter Zhen <peterzhen0713@gmail.com>
2017-03-23 12:40:03 -0400

    add zip and icons

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-u
 var QUESTIONS = [
     "Given a list of integers, find the highest product you can get from three of the integers.",
     "You have a list of integers, and for each index you want to find the product of every integer except the integer at that index.",
-    "Write a function to see if a binary tree ↴ is superbalanced.  A tree is superbalanced if the difference between the depths of any two leaf nodes ↴ is no greater than one.",
+    "Write a function to see if a binary tree is superbalanced.  A tree is superbalanced if the difference between the depths of any two leaf nodes ↴ is no greater than one.",
     "Write a function that takes an interger n and returns the nth fibonacci number.",
     "Write a function to check if a given integer is prime",
-    "Suppose we had a list ↴ of nn integers sorted in ascending order. How quickly could we check if a given integer is in the list?"
+    "Suppose we had a list of n integers sorted in ascending order. How quickly could we check if a given integer is in the list?"
 ];
 
 /**
